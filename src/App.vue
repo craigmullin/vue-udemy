a09d2105771786dd3b80077365d8827f9f2dc984
Craig Mullin <cmullin@dreeshomes.com>
2020-03-01 08:38:45 -0500

    add axios and YouTube API key

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
 <div>
-  Hi There!
   <SearchBar @termChange="onTermChange"></SearchBar>
- 
+  <VideoList></VideoList>
 </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import axios from 'axios';
+const API_KEY = 'AIzaSyD0Oeedax3t5CqF3Fzab87enLJS1pbYbHI';
 export default {
   //include properties that 
   //customize this component
   name: 'App',
   components: {
     SearchBar,
+    VideoList,
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => console.log(response))
     }
   }
 }
